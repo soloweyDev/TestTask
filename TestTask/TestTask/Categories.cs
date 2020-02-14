10f8964c4183a14f4ff80b734d5d7c82a356c827
Ануфриев Дмитрий <da@en-gy.ru>
2020-02-14 16:19:41 +0300

    Создал проект и 2 класса для заданий 1 и 2

---
+++ TestTask/TestTask/Categories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TestTask
+{
+    [Serializable]
+    [XmlRoot]
+    public class Categories
+    {
+        [XmlArray]
+        public IEnumerable<Category> categories;
+
+        [Serializable]
+        public class Category
+        {
+            [XmlAttribute]
+            public string id;
+            [XmlAttribute]
+            public string name;
+            [XmlAttribute]
+            public string parent;
+            [XmlAttribute]
+            public string image;
+        }
+    }
+}
---
+++ TestTask/TestTask/ErrorCodes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TestTask
+{
+    [Serializable]
+    [XmlRoot]
+    public class ErrorCodes
+    {
+        [XmlArray]
+        public IEnumerable<ErrorCode> errorCodes;
+
+        [Serializable]
+        public class ErrorCode
+        {
+            [XmlAttribute]
+            public string code;
+            [XmlAttribute]
+            public string text;
+        }
+    }
+}
