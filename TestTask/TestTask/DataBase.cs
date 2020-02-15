458807d1838cd0dfdd77cf0fde0a4d5adf110e5e
soloweyDev <solowey@narod.ru>
2020-02-15 13:59:50 +0300

    Допавил работу с бд

---
+++ TestTask/TestTask/DataBase.cs
@@ -0,0 +1,109 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace TestTask
+{
+    public class DataBase
+    {
+        private readonly string _connectionString;
+        private SqlConnection _sqlConnection = null;
+
+        // Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
+        public DataBase() : this(@"Data Source = localhost\SQLEXPRESS;Integrated Security=true;Initial Catalog=TestTask")
+        {
+        }
+
+        public DataBase(string connectionString) => _connectionString = connectionString;
+
+        private void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection { ConnectionString = _connectionString };
+            _sqlConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            if (_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+        }
+
+        public void InsertCategory(Categories.Category category)
+        {
+            OpenConnection();
+
+            using (var command = new SqlCommand("AddCategories", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                var parameter = new SqlParameter
+                {
+                    ParameterName = "@Id",
+                    Value = category.id,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Name",
+                    Value = category.name,
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 255
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Parent",
+                    Value = category.parent,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Image",
+                    Value = category.image,
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 255
+                };
+                command.Parameters.Add(parameter);
+
+                command.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+
+        public void InsertErrorCode(ErrorCodes.ErrorCode errorCode)
+        {
+            OpenConnection();
+
+            using (var command = new SqlCommand("AddErrorCodes", _sqlConnection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                var parameter = new SqlParameter
+                {
+                    ParameterName = "@Code",
+                    Value = errorCode.code,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@TextIn",
+                    Value = errorCode.text,
+                    SqlDbType = SqlDbType.NVarChar,
+                    Size = 255
+                };
+                command.Parameters.Add(parameter);
+
+                command.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+    }
+}
