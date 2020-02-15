7cf43efd9bc1958af5f9a2aef8b2befcdd3645ee
soloweyDev <solowey@narod.ru>
2020-02-15 13:59:20 +0300

    Допил работы с sql

--- SQL/AddCategories.sql
+++ SQL/AddCategories.sql
@@ -4,17 +4,13 @@ CREATE PROCEDURE AddCategories
 	@Id INT, 
 	@Name NVARCHAR(255),
 	@Parent INT,
-	@Image NVARCHAR(255),
-	@outTest NVARCHAR(20) OUTPUT
+	@Image NVARCHAR(255)
 AS
 BEGIN
-	IF EXISTS (SELECT * from Categories WHERE Id = @Id)
-		SET @outTest = 'Already exists'
-	ELSE
+	IF NOT EXISTS (SELECT * from Categories WHERE Id = @Id)
 	BEGIN
 		INSERT INTO Categories(Id, Name, Parent, Image)
 		VALUES(@Id, @Name, @Parent, @Image)
-		SET @outTest = 'Added'
 	END;
 END;
 GO
--- SQL/AddErrorCode.sql
+++ SQL/AddErrorCode.sql
@@ -2,17 +2,13 @@ USE TestTask;
 GO
 CREATE PROCEDURE AddErrorCodes 
 	@Code INT, 
-	@TextIn NVARCHAR(255),
-	@outTest NVARCHAR(20) OUTPUT
+	@TextIn NVARCHAR(255)
 AS
 BEGIN
-	IF EXISTS (SELECT * from ErrorCodes WHERE Code = @Code)
-		SET @outTest = 'Already exists'
-	ELSE
+	IF NOT EXISTS (SELECT * from ErrorCodes WHERE Code = @Code)
 	BEGIN
 		INSERT INTO ErrorCodes(Code, TextIn)
 		VALUES(@Code, @TextIn)
-		SET @outTest = 'Added'
 	END;
 END;
 GO
